73d4cb2e8fb1aa8d59a8ebfc32dd43953658edfe
Marija Petrevska <marija_petrevska25@yahoo.com>
2025-07-01 13:19:11 +0200

    Models updated

---
+++ Class04(Views_In_ASP.NET)/Homework_2/Homework2/Models/DtoModel/CourseWithDetailsDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework2.Models.DtoModel
+{
+    public class CourseWithDetailsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NumberOfClasses { get; set; }
+
+        public CourseWithDetailsDto(int id, string name, int numberOfClasses)
+        {
+            Id = id;
+            Name = name;
+            NumberOfClasses = numberOfClasses;
+        }
+    }
+}
